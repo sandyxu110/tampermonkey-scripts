f43739bdac056299e23cf88a8705d7f1269439cd
sandyxu110 <51484796+sandyxu110@users.noreply.github.com>
2025-10-21 14:33:32 +0800

    Update en_paste.meta.js

--- en_paste.meta.js
+++ en_paste.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Credamo 粘贴助手
 // @namespace    https://tampermonkey-scripts-eun.pages.dev
-// @version      1.7
-// @description  仅在页面存在 <input type="text"> 时显示按钮，粘贴内容到文本框
+// @version      1.8
+// @description  粘贴内容到文本框
 // @author       feng + Copilot
 // @match        https://www.credamo.com/answer.html*
 // @updateURL    https://tampermonkey-scripts-eun.pages.dev/en_paste.meta.js
