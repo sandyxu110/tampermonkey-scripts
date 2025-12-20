8cd8609f381569f807d772e330454e80b3578cee
sandyxu110 <51484796+sandyxu110@users.noreply.github.com>
2025-12-20 22:22:10 +0800

    Create time.meta.js

---
+++ time.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         右上角点击显示系统时间
+// @namespace    http://tampermonkey.net/
+// @version      1.3
+// @description  右上角半透明按钮，点击才显示系统时间 HH:mm:ss
+// @match           *://www.credamo.cc/answer*
+// @match           *://www.credamo.com/answer*
+// @updateURL    https://tampermonkey-scripts-eun.pages.dev/time.meta.js
+// @downloadURL  https://tampermonkey-scripts-eun.pages.dev/time.user.js
+// @grant        none
+// ==/UserScript==
