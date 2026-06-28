29ac91c405de042cc1353c39b0b1cbb1538cca4f
sandyxu110 <51484796+sandyxu110@users.noreply.github.com>
2026-06-28 11:51:15 +0800

    Remove user script metadata from Screen.meta.js

--- Screen.meta.js
+++ Screen.meta.js
@@ -1,13 +1,1 @@
-// ==UserScript==
-// @name         虚拟网页分辨率
-// @namespace    https://tampermonkey-scripts-eun.pages.dev
-// @version      1.0
-// @description  随机虚拟分辨率
-// @author       Sam
-// @match        https://www.credamo.com/answer.html*
-// @match        https://www.credamo.cc/answer.html*
-// @run-at       document-start
-// @updateURL    https://tampermonkey-scripts-eun.pages.dev/screen.meta.js
-// @downloadURL  https://tampermonkey-scripts-eun.pages.dev/screen.user.js
-// @grant        none
-// ==/UserScript==
+
